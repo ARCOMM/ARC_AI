e59dae1187bb352361eafc3409cd86d901112700
James Kingsley <jameslkingsley@outlook.com>
2016-04-08 21:48:24 +0100

    dfgdf

--- addons/arc_ai_skill/config.cpp
+++ addons/arc_ai_skill/config.cpp
@@ -11,14 +11,14 @@ class CfgPatches {
 };
 
 class CfgAISkill {
-    aimingAccuracy[] = {0,0.15,1,0.15}; // Values of 0 causes AI to shoot at their feet
+    aimingAccuracy[] = {0,0.05,1,0.05}; // Values of 0 causes AI to shoot at their feet
     aimingShake[] = {0,0.1,1,0.1};
-    aimingSpeed[] = {0,0.75,1,0.75};
+    aimingSpeed[] = {0,0.5,1,0.5};
     commanding[] = {0,1,1,1};
     courage[] = {0,1,1,1};
     endurance[] = {0,1,1,1};
     general[] = {0,1,1,1};
     reloadSpeed[] = {0,1,1,1};
-    spotDistance[] = {0,1,1,1};
-    spotTime[] = {0,1,1,1};
+    spotDistance[] = {0,0.5,1,0.5};
+    spotTime[] = {0,0.75,1,0.75};
 };
