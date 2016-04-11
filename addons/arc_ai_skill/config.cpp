87dda424e4cb8a7727f36654105757ea537f89de
James Kingsley <jameslkingsley@outlook.com>
2016-04-11 03:01:10 +0100

    easier to edit on server

--- addons/arc_ai_skill/config.cpp
+++ addons/arc_ai_skill/config.cpp
@@ -10,15 +10,4 @@ class CfgPatches {
     };
 };
 
-class CfgAISkill {
-    aimingAccuracy[] = {0,0.05,1,0.05}; // Values of 0 causes AI to shoot at their feet
-    aimingShake[] = {0,0.1,1,0.1};
-    aimingSpeed[] = {0,0.5,1,0.5};
-    commanding[] = {0,1,1,1};
-    courage[] = {0,1,1,1};
-    endurance[] = {0,1,1,1};
-    general[] = {0,1,1,1};
-    reloadSpeed[] = {0,1,1,1};
-    spotDistance[] = {0,0.5,1,0.5};
-    spotTime[] = {0,0.75,1,0.75};
-};
+#include "\userconfig\arc_ai\skill.hpp"
---
+++ userconfig/arc_ai/skill.hpp
@@ -0,0 +1,12 @@
+class CfgAISkill {
+    aimingAccuracy[] = {0,0.075,1,0.075}; // Values of 0 causes AI to shoot at their feet
+    aimingShake[] = {0,0.1,1,0.1};
+    aimingSpeed[] = {0,0.25,1,0.25};
+    commanding[] = {0,1,1,1};
+    courage[] = {0,1,1,1};
+    endurance[] = {0,1,1,1};
+    general[] = {0,1,1,1};
+    reloadSpeed[] = {0,1,1,1};
+    spotDistance[] = {0,1,1,1};
+    spotTime[] = {0,1,1,1};
+};
