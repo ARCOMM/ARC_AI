5312a53530cf567f0eac525cffb91439948310c9
James Kingsley <jameslkingsley@outlook.com>
2018-01-28 21:04:37 +0000

    Add CUP

--- addons/arc_ai_client_launchers/macros.hpp
+++ addons/arc_ai_client_launchers/macros.hpp
@@ -7,5 +7,5 @@
         allowAgainstInfantry = 1;\
         soundFly[] = {"A3\Sounds_F\weapons\Rockets\rocket_fly_1",6,1.5,400};\
         whistleDist = 24;\
-        cost = 2;\
+        cost = LAUNCHER_COST;\
     }
--- userconfig/arc_ai/macros.hpp
+++ userconfig/arc_ai/macros.hpp
@@ -2,3 +2,5 @@
 #define BURST_COUNT "2 + round random 3"
 #define FULL_COUNT "5 + round random 3"
 #define MG_COUNT "10 + round random 6"
+
+#define LAUNCHER_COST 1
